1583183aa25ceabc2cf1bbace6a0cfcdb3e123dd
Masaya Kazama <miyan@t-p.jp>
2019-06-10 21:39:18 +0900

    add tabkey to numlayer

--- keyboards/claw44/keymaps/miyaoka/keymap.c
+++ keyboards/claw44/keymaps/miyaoka/keymap.c
@@ -109,9 +109,9 @@ _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR
                   _______, KC_M_B3, KC_M_B1, KC_M_B2,  _______, _______, _______, _______
 ),
 [L_NUM] = LAYOUT( \
-KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,  DOCK   , KC_7   , KC_8   , KC_9   , _______, _______,
-KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,  KC_EQL , KC_4   , KC_5   , KC_6   , KC_ESC , _______,
-_______, _______, _______, _______, _______, _______,  KC_EQL , KC_1   , KC_2   , KC_3   , _______, _______,
+_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______,  DOCK   , KC_7   , KC_8   , KC_9   , _______, _______,
+_______, KC_F5  , KC_F6,   KC_F7  , KC_F8  , KC_TAB ,  KC_EQL , KC_4   , KC_5   , KC_6   , KC_ESC , _______,
+_______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,  KC_EQL , KC_1   , KC_2   , KC_3   , _______, _______,
                   _______, _______, _______, _______,  _______, _______, KC_0   , KC_DOT
 )
 };
